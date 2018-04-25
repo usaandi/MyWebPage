54227d6dd05b1ce7eefbc32144243e0a8fb4034d
Kaur Joonas Usai <Kaurjoonas@hot.ee>
2018-04-25 15:05:57 +0300

    Created background template

---
+++ main.js
@@ -0,0 +1,18 @@
+window.onload = initAll;
+var dotaImages = new Array("Images/dota2lanes.jpg",
+    "Images/dota2logo.jpg",
+    "Images/dota2qop.jpg",
+    "Images/dota2rubick.jpg");
+var currentImg = 0;
+
+function rotate() {
+    currentImg++;
+    if (currentImg == 4) {
+        currentImg = 0;
+    }
+    document.getElementById("imagesBackground").src = dotaImages[currentImg];
+}
+
+function initAll() {
+    setInterval("rotate()", 10000)
+}
