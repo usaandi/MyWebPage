efc516cbb699d9c77bfcce9aebfd6c430e4ee97a
Kaur Joonas Usai <Kaurjoonas@hot.ee>
2018-05-08 11:42:49 +0300

    Lisasin Countdowntimeri JS, Lisasin texti boxdivile Parandasin css

--- main.js
+++ main.js
@@ -5,6 +5,24 @@ var dotaImages = new Array("Images/dota2sf.jpg",
     "Images/dota2rubick.jpg");
 var currentImg = 0;
 
+var countDownDate = new Date("Jan 1, 2019, 00:00:00").getTime();
+
+function countdownTimer() {
+    var now = new Date().getTime();
+
+    var distance = countDownDate - now;
+
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    document.getElementById("demo").innerHTML = days + "d " + hours + "h " +
+        minutes + "m " + seconds + "s ";
+    if (distance < 0) {
+        document.getElementById("demo").innerHTML = "Head uut aastat!"
+    }
+}
+
 function rotate() {
     currentImg++;
     if (currentImg == 4) {
@@ -14,6 +32,7 @@ function rotate() {
 }
 
 function initAll() {
-    setInterval("rotate()", 10000)
+    setInterval("rotate()", 10000);
+    setInterval("countdownTimer()", 1000);
 
 }
